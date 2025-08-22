e3a163dc9b0eb14aebc2cf953d5d3fc07e070ca6
Tiago Jacinto <tiagojacintodev@gmail.com>
2025-08-22 16:56:12 +0100

    chore(core): remove AppConfig

--- packages/core/src/AppConfig.ts
+++
@@ -1,5 +0,0 @@
-import type { PageConfig } from "./PageConfig";
-
-export type AppConfig = {
-	[key: string]: PageConfig | AppConfig;
-};
--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,4 +1,3 @@
-export * from "./AppConfig";
 export * from "./ElementConfig";
 export * from "./PageConfig";
 export * from "./PageController";
